20d17da96d68bd3b4268faa4c910818dafdd261a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-02 18:01:37 +0700

    feat(widget): add Form to send data to user page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_routing/layouts/static_layout.dart';
 
+class Form extends StatefulWidget {
+  const Form({Key? key}) : super(key: key);
+
+  @override
+  _FormState createState() => _FormState();
+}
+
+class _FormState extends State<Form> {
+  String input = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+          child: TextField(
+            onChanged: (value) => setState(() => input = value),
+          )
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print(input);
+          },
+          child: const Text("Send Data")
+        )
+      ],
+    );
+  }
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -8,6 +40,8 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return StaticLayout(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
@@ -17,9 +51,12 @@ class HomePage extends StatelessWidget {
             onPressed: () {
               Navigator.pushNamed(context, '/about');
             },
-            child: const Text(
-              "About"
-            )
+            child: const Text("About")
+          ),
+          Container(
+            width: 300,
+            margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+            child: const Form(),
           )
         ],
       ),
