49dd421a0c3169985a076234d6bb748be794a8a3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-02 16:58:27 +0700

    feat(pages): add initial home and about pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_routing/pages/about_page.dart';
+import 'package:flutter_routing/pages/home_page.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Text("Hello world !"),
-    )
-  );
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/home': (context) => const HomePage(),
+      '/about': (context) => const AboutPage()
+    },
+  ));
 }
---
+++ lib/pages/about_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("About Page"),
+    );
+  }
+}
